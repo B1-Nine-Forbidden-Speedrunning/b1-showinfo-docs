2fbe32902033eb9f160bfe165a4524800c71b43a
DavidingPlus <davidingplus@qq.com>
2025-09-09 20:10:50 +0800

    facing more bugs, use vitepress-theme-website instead

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,12 +2,15 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import ThemeLayout from './layout.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: ThemeLayout,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
   enhanceApp({ app, router, siteData }) {
     // ...
   }
